0a7faaf6334637d9789e26469c02939c7f7e7a93
Jason Walsh <github@jwalsh.net>
2011-12-18 13:04:05 -0800

    Base setting for https://github.com/pallet/ritz

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject exercises-4clojure "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[ritz "0.2.0"]
+		     [org.clojure/clojure "1.3.0"]])
