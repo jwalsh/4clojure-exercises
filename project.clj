c6c4fc56e97fdc1d03dcc96e536582480a737954
Jason Walsh <github@jwalsh.net>
2011-12-18 13:45:07 -0800

    Additional Swank support

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[ritz "0.2.0"]
-		     [org.clojure/clojure "1.3.0"]])
+		     [org.clojure/clojure "1.3.0" :classifier "sources"]])
