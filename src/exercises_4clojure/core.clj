205d85e1fd63f059f103e64efbdc86a18424556c
Jason Walsh <jason.walsh@memorylane.com>
2011-12-18 17:23:27 -0800

    Whitespace cleanup

--- src/exercises_4clojure/core.clj
+++ src/exercises_4clojure/core.clj
@@ -4,24 +4,30 @@
 (defn nth-element-solution [l n]
   (first (drop n l)))
 
+
 ;; 20
 (defn penultimate-element-solution [l] 
   (second (reverse l)))
+
 
 ;; 34
 ;; Write a function which creates a list of all integers in a given range.
 (defn implement-range-solution [b e]
   (range b e))
 
+
 ;; 27
 ;; Write a function which returns true if the given sequence is a palindrome.
 (defn palindrome-detector-solution [s]
   (= (seq s) (reverse s)))
 
+
 ;; 32
 ;; Write a function which duplicates each element of a sequence.
 (defn duplicate-a-sequence-solution [s]
   (mapcat #(repeat 2 %) s))
+
+
 ;; 42
 ;; Write a function which calculates factorials.
 (defn factorial-fun-solution [x]
