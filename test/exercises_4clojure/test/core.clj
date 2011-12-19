450bdafd7905705a3e09c9655957a31ccd6090d6
Jason Walsh <jason.walsh@memorylane.com>
2011-12-18 16:25:33 -0800

    Base cases

--- test/exercises_4clojure/test/core.clj
+++ test/exercises_4clojure/test/core.clj
@@ -2,5 +2,11 @@
   (:use [exercises-4clojure.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+
+
+(deftest can-nth-element
+(is (= (nth-element-solution '(4 5 6 7) 2) 6) " (= (nth-element-solution '(4 5 6 7) 2) 6) " )
+(is (= (nth-element-solution [:a :b :c] 0) :a) " (= (nth-element-solution [:a :b :c] 0) :a) " )
+(is (= (nth-element-solution [1 2 3 4] 1) 2) " (= (nth-element-solution [1 2 3 4] 1) 2) " )
+(is (= (nth-element-solution '([1 2] [3 4] [5 6]) 2) [5 6]) " (= (nth-element-solution '([1 2] [3 4] [5 6]) 2) [5 6]) " )
+)
