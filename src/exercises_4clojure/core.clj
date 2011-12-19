4fca8bf1a496257b43fe512f7ec95f9acefb7189
Jason Walsh <jason.walsh@memorylane.com>
2011-12-18 16:08:58 -0800

    Preparing Leiningin fore-prob

---
+++ src/exercises_4clojure/core.clj
@@ -0,0 +1,1 @@
+(ns exercises-4clojure.core)
---
+++ test/exercises_4clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns exercises-4clojure.test.core
+  (:use [exercises-4clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
