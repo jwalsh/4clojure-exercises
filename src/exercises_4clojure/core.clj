ebff4091ea3ed04cf6184375898842b4984b97fd
Jason Walsh <jason.walsh@memorylane.com>
2011-12-18 17:03:52 -0800

    Adding problem 27

--- src/exercises_4clojure/core.clj
+++ src/exercises_4clojure/core.clj
@@ -1,11 +1,19 @@
 (ns exercises-4clojure.core)
 
-; 21
+;; 21
 (defn nth-element-solution [l n]
   (first (drop n l)))
 
-; 20
+;; 20
 (defn penultimate-element-solution [l] 
   (second (reverse l)))
 
+;; 34
+;; Write a function which creates a list of all integers in a given range.
+(defn implement-range-solution [s e] ; Update args as needed!
+  (range s e))
 
+;; 27
+;; Write a function which returns true if the given sequence is a palindrome.
+(defn palindrome-detector-solution [l] ; Update args as needed!
+  (= (seq l) (reverse l)))
