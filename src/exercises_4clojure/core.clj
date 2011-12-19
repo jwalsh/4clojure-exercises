f391f8a111243406dcae2d8c895451d1092a880d
Jason Walsh <github@jwalsh.net>
2011-12-18 23:25:49 -0800

    Use regular express rather than Java utility

--- src/exercises_4clojure/core.clj
+++ src/exercises_4clojure/core.clj
@@ -61,4 +61,5 @@
 ;; Write a function which takes a string and returns a new string
 ;; containing only the capital letters.
 (defn get-the-caps-solution [s]
-  (apply str (filter #(Character/isUpperCase %) s)))
+  (apply str
+         (re-seq #"[A-Z]" s)))
